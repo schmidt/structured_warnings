797246a7773856ebb973d34b0a5585a636b2af43
Gregor Schmidt <ruby@schmidtwisser.de>
2009-11-28 13:32:38 +0100

    fixing bug, when calling warn(Warning.new())
    this should be allowed but was not

--- lib/structured_warnings/kernel.rb
+++ lib/structured_warnings/kernel.rb
@@ -40,7 +40,7 @@ module StructuredWarnings
     #
     def warn(*args)
       first = args.shift
-      if first.is_a? Class and first < Warning
+      if first.is_a? Class and first <= Warning
         warning = first 
         message = args.shift
 
