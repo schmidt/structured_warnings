0481d736aa6e77812e0dadc209e79a685de8cdb5
Gregor Schmidt <schmidt@nach-vorne.eu>
2019-03-19 11:41:01 +0100

    Support for uplevel parameter #10
    which was introduced with Ruby 2.5
    
    Also testing output in more detail, fixing some white space problems

--- lib/structured_warnings/warner.rb
+++ lib/structured_warnings/warner.rb
@@ -5,11 +5,15 @@
 class StructuredWarnings::Warner
   #  Warner.new.format(StructuredWarning::DeprecationWarning, "more info..", caller)
   #     # => "demo.rb:5 : more info.. (StructuredWarning::DeprecationWarning)"
-  def format(warning, message, stack)
+  def format(warning, message, options, stack)
     frame = stack.shift
     # This file contains the backwards compatibility code for Ruby 2.3 and
     # lower, let's skip it
     frame = stack.shift if frame.include? 'lib/structured_warnings/kernel.rb'
-    "#{frame} : #{message} (#{warning})"
+
+    # Handle introduced uplevel introduced in Ruby 2.5
+    frame = stack.shift(options[:uplevel]).last if options[:uplevel]
+
+    "#{frame}: #{message} (#{warning})\n"
   end
 end
