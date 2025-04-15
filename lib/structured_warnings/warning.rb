a9e78269b88af6e8fa6b0eb65be84dc0c4d5422c
Jan Sandbrink <nobodysnightmare@gmail.com>
2025-04-15 08:50:23 +0200

    Improve compatibility with newer Rubies
    In old Ruby versions named parameters were just a fancy way to append a hash to the list of positional arguments. Newer Ruby versions treat named parameters as something more special and they are not forwarded by `*args` anymore.

--- lib/structured_warnings/warning.rb
+++ lib/structured_warnings/warning.rb
@@ -36,7 +36,7 @@ module StructuredWarnings::Warning
   #
   #   warn StructuredWarnings::Base.new("The least specific warning you can get")
   #
-  def warn(*args)
+  def warn(*args, **options)
     first = args.shift
     if first.is_a? Class and first <= StructuredWarnings::Base
       warning = first
@@ -54,8 +54,6 @@ module StructuredWarnings::Warning
       warning = StructuredWarnings::BuiltInWarning
       message = first.to_s.split(':', 4).last[1..-2]
     end
-
-    options = args.first.is_a?(Hash) ? args.shift : {}
 
     # If args is not empty, user passed an incompatible set of arguments.
     # Maybe somebody else is overriding warn as well and knows, what to do.
